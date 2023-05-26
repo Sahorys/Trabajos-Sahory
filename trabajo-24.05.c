e1daa2e4fa48570349c152d68657208322d80b74
heyitssaho <132929986+Sahorys@users.noreply.github.com>
2023-05-25 21:12:14 -0600

    Update trabajo-24.05.c

--- trabajo-24.05.c
+++ trabajo-24.05.c
@@ -1,29 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main()
- {
- 	// esto sirve para ingresar las 5 materias :)
- 	int i,j,suma;
- 	int alm[5][5];
- 	float pro[5];
- 	printf("Ingrese la calificacion\n");
- 	for(i=0; i<5;i++){
+void ingrecali(int alm[][5]){
+	int i,j;
+    printf("Ingrese la calificacion\n");
+	for(i=0; i<5;i++){
  		printf("alumno %d",i+1);
  		for(j=0; j<5;j++){
  			printf(" Calificaciones [%d]",i+1);
  			scanf("%d",&alm[i][j]);
 		 }
-	 } //para imprimir ;0
-	 printf("Calificacion por alumno\n");
+	 } 
+}
+void calificacion(const int alm[][5]),float promedios[]){
+	int i,j,suma;
 	for(i=0; i<5;i++){
 		suma=0;
  		for(j=0; j<5;j++){
- 			suma+=alm[i][j];
- 		}
- 		pro[i]=(float) suma/5;
+			suma+=alm[i][j];
+
 		 }
-		  for(i = 0; i <5; i++) {
-		 printf("El promedio por alumno es: %.2f\n",pro[i],i+1);
-	}
+		 promedios[i]=(float) suma / 5;
+	 }	
+}
+void mp(const float promedios[]){
+	int i;
+	printf("\nPromedios: \n");
+	for(i=0; i<5;i++){
+	printf("Alumno %d; %.2f\n",i+1, promedios[i]);
+}
+}
+
+int main()
+ {
+ 	int alm[5][5];
+ 	float promedios[5];
+ 	ingrecali(alm);
+ 	calificacion(alm,promedios); 	
+ 	mp(promedios);
 	return 0;
 }
