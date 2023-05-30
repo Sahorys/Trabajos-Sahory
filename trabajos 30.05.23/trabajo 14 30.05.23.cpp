4fe7e460ec2336f93e1da52d264d3bea8cfd7891
heyitssaho <132929986+Sahorys@users.noreply.github.com>
2023-05-30 10:17:04 -0600

    Add files via upload

---
+++ trabajos 30.05.23/trabajo 14 30.05.23.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main ()
+{
+	int ventas,age,sal,iva;
+	ventas=5000;
+	age=8;
+	sal=1000;
+	iva=15;
+if(ventas<(sal*3) && age>>(10*iva));
+printf("hola es verdadero");
+//colocar parentesis
+}
