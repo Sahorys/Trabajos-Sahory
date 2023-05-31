bbe4d1bd228e0b47e51399e9bd28dc0549544f67
heyitssaho <132929986+Sahorys@users.noreply.github.com>
2023-05-31 08:09:49 -0600

    Add files via upload

---
+++ Trabajos 31.05.23/31.05.23 03.c
@@ -0,0 +1,16 @@
+/*TITULO:test de operadores ++ y --
+Nombre:Fatima Sahory:)*/
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	int m=45, n=75;
+	printf("m=%d, n=%d \n",m,n);
+	++m;
+	--n;
+	printf("m=%d, n=%d \n",m,n);
+	m++;
+	n--;
+	printf("m=%d, n=%d \n",m,n);
+	return 0;
+}
