9ec3171d057a53bda7c356f085374bf674239dad
heyitssaho <132929986+Sahorys@users.noreply.github.com>
2023-05-31 12:18:56 -0600

    Add files via upload

---
+++ Comentarios ejercicios 04.05  31.05.23/ejercicio magico 31.05 v2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	int secretoo,secreto;
+	secretoo=3;
+	printf("Ingrese su numero\n");
+	scanf("%d",&secreto);
+	if((secreto!=secretoo)?(printf("Incorrecto\n")):(printf("correcto\n")))
+	return 0;
+}
