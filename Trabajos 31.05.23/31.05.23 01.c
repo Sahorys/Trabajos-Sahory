cc61af7e0c4c69f24c8f785ccfe1300b211eeff2
heyitssaho <132929986+Sahorys@users.noreply.github.com>
2023-05-31 07:49:59 -0600

    Add files via upload

---
+++ Trabajos 31.05.23/31.05.23 01.c
@@ -0,0 +1,11 @@
+/*TITULO:imprime el tamaño de valores de coma flotante y double
+Nombre:Fatima Sahory:)*/
+#include <stdio.h>
+int main()
+ {
+ 	printf("El tamaño de variables de coma flotante es %d", sizeof (float));
+ 	printf(" bytes\n");
+ 	printf("el tamaño de variables de doble precision es %d", sizeof (double));
+ 	printf(" bytes\n");
+	return 0;
+}
