ef33950f2065cefe5eeabb55823c552eb0520dbc
heyitssaho <132929986+Sahorys@users.noreply.github.com>
2023-06-05 10:54:48 -0600

    Update 05.06.23 02.c

--- 05.06.23/05.06.23 02.c
+++ 05.06.23/05.06.23 02.c
@@ -5,10 +5,10 @@ int main()
 	char c;
 	switch(c)
 	{
-	case:'0' :case'1' :case'2' :case'3' :case'4'
-	case:'5' case'6':case'7' case'8':case'9'
+	case:'0' case'1': :case:'2' case:'3' case:'4':
+	case:'5' case'6':case'7' case'8':case'9':
 	num_digitos++;
-	case' ':case'\t':case'\n';
+	case' ':case'\t':case'\n':
 	num_blancos++;
 	printf ("%c ",num_digitos);
 	printf ("%c ",num_blancos);
