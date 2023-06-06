fe656a2ab6865a1eddbc1016f0c82e5cd9985778
heyitssaho <132929986+Sahorys@users.noreply.github.com>
2023-06-06 10:42:35 -0600

    Add files via upload

---
+++ trabajos 06.03.23/06..03.23 07.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()	//con llaves llega del 1 al 24--- sin llaves es un 1 infinito
+{
+	int contador=1;
+	while (contador<25)
+	{
+	printf("%d\n",contador);
+	contador++;
+}
+	return 0;
+}
