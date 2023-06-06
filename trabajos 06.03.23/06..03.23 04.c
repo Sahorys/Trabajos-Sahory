b62602ce1c06e1a68cf0ef4f42f9606f8c42eb6a
heyitssaho <132929986+Sahorys@users.noreply.github.com>
2023-06-06 10:00:00 -0600

    Add files via upload

---
+++ trabajos 06.03.23/06..03.23 04.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int contador=1;
+	while(contador>100)//si la prueba logica es falsa,
+	// no se ejecuta la sentencia de instruccion
+	{
+		printf("%d\n",contador);
+		contador++;
+	}
+}
