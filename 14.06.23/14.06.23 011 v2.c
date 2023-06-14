4da43b1e5733d55e6d741d1adea5ec036d6bddee
heyitssaho <132929986+Sahorys@users.noreply.github.com>
2023-06-14 08:27:12 -0600

    Add files via upload

---
+++ 14.06.23/14.06.23 011 v2.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+	int numero,primo=0,i;
+	char a;
+	do
+	{
+	printf("Ingrese numero");
+	scanf("%d",&numero);	
+	for ( i=1; i<=numero; i++)
+	{
+		if(numero % i == 0)
+		{
+			primo++;
+		}
+	}
+	if(primo==2){
+		printf("Es primo\n");
+	}
+	else{
+		printf("No es primo\n");
+	}
+	printf("desea continuar\n");
+	scanf("%d",&a);
+	}while(a=='S'|| a=='s');
+	
+	return 0;
+}
